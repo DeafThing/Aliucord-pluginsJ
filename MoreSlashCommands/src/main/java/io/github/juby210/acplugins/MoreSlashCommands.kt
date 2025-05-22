dbbcbbd69587d00d31b0805dda21e33a78659a06
SeriNova <131408227+OasisVee@users.noreply.github.com>
2025-05-22 17:28:53 -0400

    Fixed MoreSlashCommands

--- MoreSlashCommands/src/main/java/io/github/juby210/acplugins/MoreSlashCommands.kt
+++ MoreSlashCommands/src/main/java/io/github/juby210/acplugins/MoreSlashCommands.kt
@@ -6,47 +6,77 @@
 package io.github.juby210.acplugins
 
 import android.content.Context
+import com.aliucord.Utils
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.api.CommandsAPI
 import com.aliucord.entities.Plugin
+import com.discord.api.commands.ApplicationCommandType
 import com.lytefast.flexinput.model.Attachment
 
 @AliucordPlugin
 @Suppress("unused")
 class MoreSlashCommands : Plugin() {
   override fun start(context: Context?) {
-    commands.registerCommand("lenny", "Appends ( ͡° ͜ʖ ͡°) to your message.", listOf(CommandsAPI.messageOption)) { ctx ->
-        CommandsAPI.CommandResult(ctx.getStringOrDefault("message", "") + " ( ͡° ͜ʖ ͡°)")
+    commands.registerCommand(
+        "lenny", 
+        "Appends ( ͡° ͜ʖ ͡°) to your message.", 
+        listOf(Utils.createCommandOption(ApplicationCommandType.STRING, "message", "The message to append lenny face to"))
+    ) { ctx ->
+        CommandsAPI.CommandResult(ctx.getString("message")!! + " ( ͡° ͜ʖ ͡°)")
     }
 
-    commands.registerCommand("mock", "Mock a user", listOf(CommandsAPI.requiredMessageOption)) { ctx ->
+    commands.registerCommand(
+        "mock", 
+        "Mock a user", 
+        listOf(Utils.createCommandOption(ApplicationCommandType.STRING, "message", "The message to mock"))
+    ) { ctx ->
         CommandsAPI.CommandResult(ctx
-            .getRequiredString("message")
+            .getString("message")!!
             .toCharArray()
             .mapIndexed { i, c -> if (i % 2 == 1) c.uppercaseChar() else c.lowercaseChar() }
             .joinToString(""))
     }
 
-    commands.registerCommand("upper", "Makes text uppercase", listOf(CommandsAPI.requiredMessageOption)) { ctx ->
-        CommandsAPI.CommandResult(ctx.getRequiredString("message").trim().uppercase())
+    commands.registerCommand(
+        "upper", 
+        "Makes text uppercase", 
+        listOf(Utils.createCommandOption(ApplicationCommandType.STRING, "message", "The text to make uppercase"))
+    ) { ctx ->
+        CommandsAPI.CommandResult(ctx.getString("message")!!.trim().uppercase())
     }
 
-    commands.registerCommand("lower", "Makes text lowercase", listOf(CommandsAPI.requiredMessageOption)) { ctx ->
-        CommandsAPI.CommandResult(ctx.getRequiredString("message").trim().lowercase())
+    commands.registerCommand(
+        "lower", 
+        "Makes text lowercase", 
+        listOf(Utils.createCommandOption(ApplicationCommandType.STRING, "message", "The text to make lowercase"))
+    ) { ctx ->
+        CommandsAPI.CommandResult(ctx.getString("message")!!.trim().lowercase())
     }
 
-    commands.registerCommand("owo", "Owoify's your text", listOf(CommandsAPI.requiredMessageOption)) { ctx ->
-        CommandsAPI.CommandResult(owoify(ctx.getRequiredString("message").trim()))
+    commands.registerCommand(
+        "owo", 
+        "Owoify's your text", 
+        listOf(Utils.createCommandOption(ApplicationCommandType.STRING, "message", "The text to owoify"))
+    ) { ctx ->
+        CommandsAPI.CommandResult(owoify(ctx.getString("message")!!.trim()))
     }
 
     val displayName = Attachment::class.java.getDeclaredField("displayName").apply { isAccessible = true }
-    commands.registerCommand("spoilerfiles", "Marks attachments as spoilers", listOf(CommandsAPI.messageOption)) { ctx ->
+    commands.registerCommand(
+        "spoilerfiles", 
+        "Marks attachments as spoilers", 
+        listOf(Utils.createCommandOption(ApplicationCommandType.STRING, "message", "Optional message to send with spoiler files"))
+    ) { ctx ->
       for (a in ctx.attachments) displayName[a] = "SPOILER_" + a.displayName
-        CommandsAPI.CommandResult(ctx.getStringOrDefault("message", ""))
+        CommandsAPI.CommandResult(ctx.getString("message") ?: "")
     }
 
-    commands.registerCommand("reverse", "Makes text reversed", listOf(CommandsAPI.requiredMessageOption)) { ctx ->
-        CommandsAPI.CommandResult(ctx.getRequiredString("message").reversed())
+    commands.registerCommand(
+        "reverse", 
+        "Makes text reversed", 
+        listOf(Utils.createCommandOption(ApplicationCommandType.STRING, "message", "The text to reverse"))
+    ) { ctx ->
+        CommandsAPI.CommandResult(ctx.getString("message")!!.reversed())
     }
   }
 
